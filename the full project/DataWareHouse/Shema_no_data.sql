165bab9ce0c002f87d8c23ba323ff7552c2f5032
Mohammedbasta <simobasta01@gmail.com>
2018-11-07 23:20:37 +0100

    Add files via upload

---
+++ the full project/DataWareHouse/Shema_no_data.sql
@@ -0,0 +1,29 @@
+-- --------------------------------------------------------
+-- Hôte :                        127.0.0.1
+-- Version du serveur:           5.6.17 - MySQL Community Server (GPL)
+-- SE du serveur:                Win64
+-- HeidiSQL Version:             9.3.0.4984
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+-- L'exportation de données n'était pas sélectionnée.
+
+-- L'exportation de données n'était pas sélectionnée.
+
+-- L'exportation de données n'était pas sélectionnée.
+
+-- L'exportation de données n'était pas sélectionnée.
+
+-- L'exportation de données n'était pas sélectionnée.
+
+-- L'exportation de données n'était pas sélectionnée.
+
+-- L'exportation de données n'était pas sélectionnée.
+
+-- L'exportation de données n'était pas sélectionnée.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
